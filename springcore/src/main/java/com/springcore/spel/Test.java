56932b3955f3f8723f9ef338e16546f11064d6fd
KD <KD@KD>
2023-10-12 19:57:02 +0530

    added new package com.springcore.spel from windows eclipse

---
+++ springcore/src/main/java/com/springcore/spel/Test.java
@@ -0,0 +1,22 @@
+package com.springcore.spel;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/spel/spelconfig.xml");
+		Demo d = context.getBean("demo",Demo.class);
+		System.out.println(d);
+		
+		SpelExpressionParser temp = new SpelExpressionParser();
+		org.springframework.expression.Expression ep =  temp.parseExpression("2+3");
+		System.out.println(ep.getValue());
+	}
+
+}
